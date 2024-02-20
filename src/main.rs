5473ea778b30a0de6138339039f4e93d538f3448
Teddy Leung <leungteddy12@gmail.com>
2024-02-20 20:11:45 +0800

    save to vector

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
 
 fn main() {
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error");
+
     let host = cpal::default_host();
     let device = host
         .default_input_device()
@@ -15,10 +24,15 @@ fn main() {
         .expect("no suported config?!")
         .with_max_sample_rate();
 
+    let audio_data = Arc::new(Mutex::new(Vec::<i16>::new()));
+
+    let cloned_data = Arc::clone(&audio_data);
+
     let stream = device
         .build_input_stream(
             &supported_config.into(),
             move |data: &[i16], info: &cpal::InputCallbackInfo| {
+                cloned_data.lock().unwrap().extend_from_slice(data);
                 println!("{:?}", info.timestamp().capture)
             },
             move |err| {},
@@ -26,7 +40,9 @@ fn main() {
         )
         .expect("cannot build the stream");
 
-    loop {
+    while running.load(Ordering::SeqCst) {
         stream.play().unwrap();
     }
+
+    println!("{:?}", audio_data.lock().unwrap());
 }
